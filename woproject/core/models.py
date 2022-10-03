50333a0be3fa4dd20e92c3d9e68a694309907f97
root <pkverma131@gmail.com>
2022-10-03 15:13:48 +0500

    apps added

---
+++ woproject/core/models.py
@@ -0,0 +1,86 @@
+from django.db import models
+
+class Brand(models.Model):
+    name = models.CharField(max_lenght=100)
+    logo =  models.FileField()
+    company = models.CharField(max_length=100)
+    origin = models.CharField(max_lenght=100)
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class ApplianceType(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class Specification(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class Appliance(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class ApplicianceSpecificationAsso(models.Model):
+    appliances_id = models.IntegerField()
+    specification_id = models.IntegerField()
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class Seller(models.Model):
+    name = models.CharField(max_length=100)
+    channel= models.CharField(max_length=100)
+    site_url = models.CharField(max_length=100)
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class ApplianceSellerPriceAsso(models.Model):
+    appliance_id = models.IntegerField()
+    seller_id = models.IntegerField()
+    price = models.FloatField()
+    listing_url = models.CharField(max_length=100)
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class Offer(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    desccription = models.Model(max_length=200)
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class OfferAttributes(models.Model):
+    name = models.CharField()
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+    
+class OfferAttributesAsso():
+    offer_id = models.IntegerField()
+    offer_attributes_id = models.IntegerField()
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
---
+++ woproject/lead/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+class State(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class City(models):
+    state_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
+
+class Lead(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    pincode = models.IntegerField()
+    city = models.IntegerField()
+    state = models.IntegerField()
+    created_by = models.IntegerField()
+    create_at = models.DateTimeField()
+    updated_by = models.IntegerField()
+    updated_at = models.DateTimeField()
